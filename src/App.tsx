e392dd94f96c811fb99fe11aef277c8d73c80190
litophoto <ciro17581@gmail.com>
2024-05-12 16:36:43 +0900

    fix: 使ってない要素を参照しないように修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,6 @@
-import { useState, useEffect } from "react";
-import { db } from "./infrastructure/firebase";
-import { collection, getDocs } from "firebase/firestore";
 import ComponentBlock from "./features/component-block/Index";
 
 function App() {
-  const [users, setUsers] = useState<any>([]);
-
-  useEffect(() => {
-    const usersCollectionRef = collection(db, "users");
-    getDocs(usersCollectionRef).then((querySnapshot) => {
-      setUsers(
-        querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
-      );
-    });
-  }, []);
-
   return (
     // <div>
     //   {users.map((user: any) => (
